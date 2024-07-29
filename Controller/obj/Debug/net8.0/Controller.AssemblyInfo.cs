6da699b9ac0b4b17ac78abd7b080d825eaea2fd9
Javier Alejandro TG <javiertomalag14@gmail.com>
2024-07-28 20:26:27 -0500

    Arreglando conflictos

--- Controller/obj/Debug/net8.0/Controller.AssemblyInfo.cs
+++ Controller/obj/Debug/net8.0/Controller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e16a485c0cfede017607893e886323ce7481525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ecc9108dae3b985f54ff2a8ccf8792beb281299")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
