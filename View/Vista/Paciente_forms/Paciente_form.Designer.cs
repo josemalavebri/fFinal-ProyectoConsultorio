702a7b7d021716d1e4a1194869b54a7d76d4bfb3
Jose <165485943+josemalavebri@users.noreply.github.com>
2024-08-04 21:51:32 -0500

    subiendo archivos completos

--- View/Vista/Paciente_forms/Paciente_form.Designer.cs
+++ View/Vista/Paciente_forms/Paciente_form.Designer.cs
@@ -47,10 +47,9 @@
             // 
             // resetear_button
             // 
-            resetear_button.Location = new Point(728, 33);
-            resetear_button.Margin = new Padding(3, 4, 3, 4);
+            resetear_button.Location = new Point(637, 25);
             resetear_button.Name = "resetear_button";
-            resetear_button.Size = new Size(106, 41);
+            resetear_button.Size = new Size(93, 31);
             resetear_button.TabIndex = 29;
             resetear_button.Text = "Resetear";
             resetear_button.UseVisualStyleBackColor = true;
@@ -58,10 +57,9 @@
             // 
             // nuevo_button
             // 
-            nuevo_button.Location = new Point(851, 24);
-            nuevo_button.Margin = new Padding(3, 4, 3, 4);
+            nuevo_button.Location = new Point(745, 18);
             nuevo_button.Name = "nuevo_button";
-            nuevo_button.Size = new Size(197, 63);
+            nuevo_button.Size = new Size(172, 47);
             nuevo_button.TabIndex = 28;
             nuevo_button.Text = "Nuevo";
             nuevo_button.UseVisualStyleBackColor = true;
@@ -69,10 +67,9 @@
             // 
             // buscar_button
             // 
-            buscar_button.Location = new Point(586, 35);
-            buscar_button.Margin = new Padding(3, 4, 3, 4);
+            buscar_button.Location = new Point(513, 26);
             buscar_button.Name = "buscar_button";
-            buscar_button.Size = new Size(110, 39);
+            buscar_button.Size = new Size(96, 29);
             buscar_button.TabIndex = 27;
             buscar_button.Text = "Buscar";
             buscar_button.UseVisualStyleBackColor = true;
@@ -80,18 +77,17 @@
             // 
             // cedula_text
             // 
-            cedula_text.Location = new Point(384, 39);
-            cedula_text.Margin = new Padding(3, 4, 3, 4);
+            cedula_text.Location = new Point(336, 29);
             cedula_text.Name = "cedula_text";
-            cedula_text.Size = new Size(168, 30);
+            cedula_text.Size = new Size(148, 25);
             cedula_text.TabIndex = 26;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(294, 44);
+            label4.Location = new Point(257, 33);
             label4.Name = "label4";
-            label4.Size = new Size(75, 21);
+            label4.Size = new Size(63, 20);
             label4.TabIndex = 25;
             label4.Text = "Cedula";
             // 
@@ -99,20 +95,19 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Century Gothic", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(81, 161);
+            label1.Location = new Point(71, 121);
             label1.Name = "label1";
-            label1.Size = new Size(206, 21);
+            label1.Size = new Size(171, 20);
             label1.TabIndex = 24;
             label1.Text = "Pacientes Agregados:";
             // 
             // paciente_dgv
             // 
             paciente_dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            paciente_dgv.Location = new Point(84, 205);
-            paciente_dgv.Margin = new Padding(3, 4, 3, 4);
+            paciente_dgv.Location = new Point(74, 154);
             paciente_dgv.Name = "paciente_dgv";
             paciente_dgv.RowHeadersWidth = 51;
-            paciente_dgv.Size = new Size(963, 255);
+            paciente_dgv.Size = new Size(843, 191);
             paciente_dgv.TabIndex = 23;
             paciente_dgv.CellClick += paciente_dgv_CellClick;
             // 
@@ -126,26 +121,29 @@
             panel3.Controls.Add(nuevo_button);
             panel3.Controls.Add(buscar_button);
             panel3.Font = new Font("Century Gothic", 10.8F);
-            panel3.Location = new Point(-1, 12);
+            panel3.Location = new Point(-1, 9);
+            panel3.Margin = new Padding(3, 2, 3, 2);
             panel3.Name = "panel3";
-            panel3.Size = new Size(1167, 111);
+            panel3.Size = new Size(1021, 83);
             panel3.TabIndex = 38;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(70, 133, 133);
             panel2.Controls.Add(pictureBox1);
-            panel2.Location = new Point(76, -7);
+            panel2.Location = new Point(66, -5);
+            panel2.Margin = new Padding(3, 2, 3, 2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(75, 96);
+            panel2.Size = new Size(66, 72);
             panel2.TabIndex = 31;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = View.Properties.Resources.Dispensario_Icon;
-            pictureBox1.Location = new Point(10, 19);
+            pictureBox1.Location = new Point(9, 14);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(54, 62);
+            pictureBox1.Size = new Size(47, 46);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -154,20 +152,23 @@
             // 
             panel1.BackColor = Color.FromArgb(70, 133, 133);
             panel1.Location = new Point(-1, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1167, 18);
+            panel1.Size = new Size(1021, 14);
             panel1.TabIndex = 37;
             // 
             // Paciente_form
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1110, 507);
+            BackColor = Color.FromArgb(222, 249, 196);
+            ClientSize = new Size(971, 380);
             Controls.Add(panel3);
             Controls.Add(panel1);
             Controls.Add(label1);
             Controls.Add(paciente_dgv);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Paciente_form";
             Text = "Pacientes";
             Load += Paciente_form_Load;
--- View/obj/Debug/net8.0-windows/View.AssemblyInfo.cs
+++ View/obj/Debug/net8.0-windows/View.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("View")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cd21ed3f17f9cd7e864812b98591dd8cfe82c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0362131510e35c00d31d25ddffdea597be0f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("View")]
 [assembly: System.Reflection.AssemblyTitleAttribute("View")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
